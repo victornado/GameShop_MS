73d31238e3778ceb4424c552d9de5e4b623ee32a
Carlos Segundo <45834017+csegundo@users.noreply.github.com>
2019-10-20 13:41:16 +0200

    commit query

--- GameShopProyect/src/Integracion/Querys/ProductsSoldDateRange.java
+++ GameShopProyect/src/Integracion/Querys/ProductsSoldDateRange.java
@@ -1,9 +1,25 @@
 package Integracion.Querys;
 
+import java.sql.Timestamp;
+
+import Transfers.TProduct;
+
 /*
  * QUERY PARA MOSTRAR TODOS LOS PRODUCTOS QUE HAN SIDO VENDIDOS EN UN RANGO DE FECHAS
- * */
+*/
 public class ProductsSoldDateRange implements Query {
+	
+	private String _queryString;
+	private TProduct _data;		// TOA ?????????
+	private Timestamp _from;
+	private Timestamp _to;
+	
+	public ProductsSoldDateRange(Object data, Timestamp from, Timestamp to) {
+		_data = (TProduct)data;
+		_from = from;
+		_to = to;
+		_queryString = "";
+	}
 
 	@Override
 	public Object execute(Object data) throws Exception {

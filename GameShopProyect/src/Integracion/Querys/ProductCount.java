e2f132402524ac49edddbdcf0975168ff5159dcc
victo <victo@192.168.0.127>
2019-11-02 14:26:30 +0100

    productCount (query) hecha

---
+++ GameShopProyect/src/Integracion/Querys/ProductCount.java
@@ -0,0 +1,36 @@
+package Integracion.Querys;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.util.Pair;
+
+public class ProductCount implements Query {
+
+	@Override
+	public Object execute(Object data) throws Exception {
+		List<Pair<String,Integer>> sol = null;
+		String queryString = "SELECT p.tipo,  COUNT(*) AS numero " + 
+							"FROM producto p " +  
+							"GROUP BY p.tipo " ;					
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + Main.Main.database,
+				Main.Main.user, Main.Main.password);
+		PreparedStatement ps = con.prepareStatement(queryString, PreparedStatement.RETURN_GENERATED_KEYS);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			sol = new ArrayList<Pair<String,Integer>>();
+			Pair<String,Integer> aux= new Pair<String,Integer>(rs.getString(1),rs.getInt(2));
+			sol.add(aux);
+		}
+		con.close();
+		return sol;
+
+	}
+	
+
+}

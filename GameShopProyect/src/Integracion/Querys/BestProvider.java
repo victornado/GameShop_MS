6a09be7db2649306a2856d1d4deee74246a94f81
victo <victo@192.168.0.127>
2019-11-02 12:13:34 +0100

    las dos Queries hechas

---
+++ GameShopProyect/src/Integracion/Querys/BestProvider.java
@@ -0,0 +1,38 @@
+package Integracion.Querys;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BestProvider implements Query {
+
+	@Override
+	public Object execute(Object data) throws Exception {
+		List<Object> sol = null;
+		String queryString = "SELECT p.nombre, pr.NIF, SUM(p.unidadesProv) AS unidades " + 
+							"FROM producto p JOIN proveedor pr " + 
+							"ON p.IDProveedor=pr.ID " + 
+							"GROUP BY pr.NIF " + 
+							"HAVING unidades >= ALL (SELECT SUM(unidadesProv) " + 
+													"FROM producto " + 
+													"GROUP BY IDProveedor) ";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + Main.Main.database,
+				Main.Main.user, Main.Main.password);
+		PreparedStatement ps = con.prepareStatement(queryString, PreparedStatement.RETURN_GENERATED_KEYS);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			sol = new ArrayList<Object>();
+			sol.add(rs.getObject(1));
+			sol.add(rs.getObject(2));
+			sol.add(rs.getObject(3));
+		}
+		con.close();
+		return sol;
+
+	}
+
+}
---
+++ GameShopProyect/src/Integracion/Querys/GetBestProductSelled.java
@@ -0,0 +1,43 @@
+package Integracion.Querys;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.util.Pair;
+
+public class GetBestProductSelled implements Query {
+
+	@Override
+	public Object execute(Object data) throws Exception {
+		@SuppressWarnings("unchecked")
+		Pair<Timestamp,Timestamp>dates=(Pair<Timestamp,Timestamp>)data;
+		List<Object[]> sol = null;
+		String queryString = "SELECT p.nombre, a.cantidad, t.fecha "
+				  + "FROM producto p JOIN asociado a "
+				  + "ON p.ID=a.IDProducto "
+				  + "JOIN ticket t "
+				  + "ON t.ID=a.IDTicket"
+				  + "WHERE t.fecha BETWEEN ? AND ? ";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + Main.Main.database, Main.Main.user, Main.Main.password);
+		PreparedStatement ps = con.prepareStatement(queryString, PreparedStatement.RETURN_GENERATED_KEYS);
+		ps.setTimestamp(1, dates.getKey());
+		ps.setTimestamp(2, dates.getValue());
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()) {
+			sol =new ArrayList<Object[]>();
+			Object aux[]= {rs.getObject(1),rs.getObject(2),rs.getObject(3),rs.getObject(4)};
+			sol.add(aux);
+		}
+		con.close();
+	
+		return sol;
+	}
+
+
+}

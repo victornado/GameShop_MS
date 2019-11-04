6a7fca8e055f9da461835f468a72167d26dafabb
victo <victo@DESKTOP-67GBJ6U>
2019-11-04 13:07:32 +0100

    lockkModeType terminado

--- GameShopProyect/src/Integracion/Querys/LockModeType.java
+++ GameShopProyect/src/Integracion/Querys/LockModeType.java
@@ -2,4 +2,6 @@ package Integracion.Querys;
 
 public class LockModeType {
 public static final Integer PESSIMISTIC=1; 
+public static final Integer OPTIMISTIC=2; 
+public static final Integer OPTIMISTIC_FORCE_INCREMENT=3; 
 }

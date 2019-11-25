7e2bce2293e9d63720d307d2d3912e658e618ea3
victornado <Victor>
2019-11-25 09:49:50 +0100

    prueba

--- GameShopProyect/src/Command/Command.java
+++ GameShopProyect/src/Command/Command.java
@@ -4,6 +4,7 @@ import javafx.util.Pair;;
 
 public abstract class Command {
 	
-	//devuelve una pair con lo que recibe del SA y el evento asociado
+	//devuelve una pair con lo que recibe del SA y el evento asociado.
 	public abstract Pair<Object, Integer> execute(Object data) throws Exception;
+	
 }

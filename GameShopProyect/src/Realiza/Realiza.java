33f16d0ab4223618a6ca537e829976760b7af1c7
Carlos Segundo <45834017+csegundo@users.noreply.github.com>
2019-12-04 21:03:44 +0100

    Añadida la carpeta Realiza

---
+++ GameShopProyect/src/Realiza/Realiza.java
@@ -0,0 +1,38 @@
+package Realiza;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.NamedQuery;
+import javax.persistence.NamedQueries;
+
+@Entity
+@NamedQueries({
+		@NamedQuery(name = "Realiza.Realiza.findByids", query = "select obj from Realiza obj where :main = obj.ids "),
+		@NamedQuery(name = "Realiza.Realiza.findByduracion", query = "select obj from Realiza obj where :duracion = obj.duracion ") })
+public class Realiza implements Serializable {
+	private static final long serialVersionUID = 0;
+	
+	@EmbeddedId
+	private RealizaEmbeddable ids;
+	private Integer duracion;
+
+	public Realiza() {
+	}
+	
+	public RealizaEmbeddable getIds() {
+		return ids;
+	}
+	
+	public void setIds(RealizaEmbeddable ids) {
+		this.ids = ids;
+	}
+	
+	public Integer getDuracion() {
+		return duracion;
+	}
+	
+	public void setDuracion(Integer duracion) {
+		this.duracion = duracion;
+	}
+}
---
+++ GameShopProyect/src/Realiza/RealizaEmbeddable.java
@@ -0,0 +1,67 @@
+package Realiza;
+
+import java.io.Serializable;
+import javax.persistence.Embeddable;
+import java.util.UUID;
+
+@Embeddable
+public class RealizaEmbeddable implements Serializable {
+
+	private static final long serialVersionUID = 0;
+	
+	private Integer Empleado;
+	private Integer Conferencia;
+
+	public RealizaEmbeddable() {
+	}
+
+	public Integer getConferencia() {
+		return Conferencia;
+	}
+
+	public void setConferencia(Integer Conferencia) {
+		this.Conferencia = Conferencia;
+	}
+
+	public Integer getEmpleado() {
+		return Empleado;
+	}
+
+	public void setEmpleado(Integer Empleado) {
+		this.Empleado = Empleado;
+	}
+
+	public boolean equals(Object obj) {
+		if (obj == this)
+			return true;
+		if (!(obj instanceof RealizaEmbeddable))
+			return false;
+		RealizaEmbeddable pk = (RealizaEmbeddable) obj;
+		if ((Conferencia == null && pk.Conferencia != null)
+				|| (Conferencia != null && !Conferencia.equals(pk.Conferencia)))
+			return false;
+		if ((Empleado == null && pk.Empleado != null) || (Empleado != null && !Empleado.equals(pk.Empleado)))
+			return false;
+		return true;
+	}
+
+	private UUID uuid;
+
+	public int hashCode() {
+		final int prime = 31;
+		int hash = 17;
+		if (Conferencia != null) {
+			hash = hash * prime + Conferencia.hashCode();
+		}
+		if (Empleado != null) {
+			hash = hash * prime + Empleado.hashCode();
+		}
+		if (hash == 17) {
+			if (uuid == null) {
+				uuid = UUID.randomUUID();
+			}
+			hash = uuid.hashCode();
+		}
+		return hash;
+	}
+}

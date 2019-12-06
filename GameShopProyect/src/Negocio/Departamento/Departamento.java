6b94ee84c7fdb7d6592114956cfc79705e8c8262
victor <victor@DESKTOP-T8OJ1L4>
2019-12-06 11:20:31 +0100

    algunos toTrasnfer

--- GameShopProyect/src/Negocio/Departamento/Departamento.java
+++ GameShopProyect/src/Negocio/Departamento/Departamento.java
@@ -10,12 +10,13 @@ import javax.persistence.NamedQuery;
 import javax.persistence.NamedQueries;
 import Negocio.Transfers.TDepartamento;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author carlo
-* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-*/
+/**
+ * <!-- begin-UML-doc --> <!-- end-UML-doc -->
+ * 
+ * @author carlo
+ * @generated "UML a JPA
+ *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+ */
 @Entity
 @NamedQueries({
 		@NamedQuery(name = "Negocio.Departamento.Departamento.findByid", query = "select obj from Departamento obj where :id = obj.id "),
@@ -24,163 +25,181 @@ import Negocio.Transfers.TDepartamento;
 		@NamedQuery(name = "Negocio.Departamento.Departamento.findByfacturacion", query = "select obj from Departamento obj where :facturacion = obj.facturacion "),
 		@NamedQuery(name = "Negocio.Departamento.Departamento.findByplanta", query = "select obj from Departamento obj where :planta = obj.planta ") })
 public class Departamento implements Serializable {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * <!-- begin-UML-doc --> <!-- end-UML-doc -->
+	 * 
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	private static final long serialVersionUID = 0;
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * <!-- begin-UML-doc --> <!-- end-UML-doc -->
+	 * 
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public Departamento() {
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * <!-- begin-UML-doc --> <!-- end-UML-doc -->
+	 * 
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	@Id
 	private Integer id;
 
-	/** 
-	* @return the id
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * @return the id
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public Integer getId() {
 		return id;
 	}
 
-	/** 
-	* @param id the id to set
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * @param id the id to set
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * <!-- begin-UML-doc --> <!-- end-UML-doc -->
+	 * 
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	private String nombre;
 
-	/** 
-	* @return the nombre
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * @return the nombre
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public String getNombre() {
 		// begin-user-code
 		return nombre;
 		// end-user-code
 	}
 
-	/** 
-	* @param nombre the nombre to set
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * @param nombre the nombre to set
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public void setNombre(String nombre) {
 		// begin-user-code
 		this.nombre = nombre;
 		// end-user-code
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * <!-- begin-UML-doc --> <!-- end-UML-doc -->
+	 * 
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	private Integer numEmpleados;
 
-	/** 
-	* @return the numEmpleados
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * @return the numEmpleados
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public Integer getNumEmpleados() {
 		// begin-user-code
 		return numEmpleados;
 		// end-user-code
 	}
 
-	/** 
-	* @param numEmpleados the numEmpleados to set
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * @param numEmpleados the numEmpleados to set
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public void setNumEmpleados(Integer numEmpleados) {
 		// begin-user-code
 		this.numEmpleados = numEmpleados;
 		// end-user-code
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * <!-- begin-UML-doc --> <!-- end-UML-doc -->
+	 * 
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	private Double facturacion;
 
-	/** 
-	* @return the facturacion
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * @return the facturacion
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public Double getFacturacion() {
 		// begin-user-code
 		return facturacion;
 		// end-user-code
 	}
 
-	/** 
-	* @param facturacion the facturacion to set
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * @param facturacion the facturacion to set
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public void setFacturacion(Double facturacion) {
 		// begin-user-code
 		this.facturacion = facturacion;
 		// end-user-code
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * <!-- begin-UML-doc --> <!-- end-UML-doc -->
+	 * 
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	private Integer planta;
 
-	/** 
-	* @return the planta
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * @return the planta
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public Integer getPlanta() {
 		// begin-user-code
 		return planta;
 		// end-user-code
 	}
 
-	/** 
-	* @param planta the planta to set
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * @param planta the planta to set
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public void setPlanta(Integer planta) {
 		// begin-user-code
 		this.planta = planta;
 		// end-user-code
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a JPA (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
-	*/
+	/**
+	 * <!-- begin-UML-doc --> <!-- end-UML-doc -->
+	 * 
+	 * @return
+	 * @generated "UML a JPA
+	 *            (com.ibm.xtools.transform.uml2.ejb3.java.jpa.internal.UML2JPATransform)"
+	 */
 	public TDepartamento toTransfer() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		TDepartamento sol = new TDepartamento(this.getNombre(), this.getFacturacion(), this.getNumEmpleados(),
+				this.getPlanta());
+		sol.setID(this.getId());
+		return sol;
 	}
 }

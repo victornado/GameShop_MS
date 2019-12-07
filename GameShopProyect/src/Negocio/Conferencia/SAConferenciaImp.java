54c588993b899732655098f55d10eff3edc2c659
Viitali <vsavchen@ucm.es>
2019-12-07 20:10:51 +0100

    Conferencia actualizada

--- GameShopProyect/src/Negocio/Conferencia/SAConferenciaImp.java
+++ GameShopProyect/src/Negocio/Conferencia/SAConferenciaImp.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 public class SAConferenciaImp implements SAConferencia {
 	
@@ -29,18 +30,47 @@ public class SAConferenciaImp implements SAConferencia {
 	}
 
 	public Boolean eliminarConferencia(Integer id) {
-		return null;
+		Conferencia con = findConferencia(id);
+		if(con!=null)
+		{
+			em.remove(con);
+			return true;
+		}
+		return false;
 	}
 
 	public Boolean modificarConferencia(TConferencia data) {
-		return null;
+		
+		//Al reactivar conferencia, como accedemos a la variable 'activo'
+		
+		Conferencia con = findConferencia(data.getID());
+		if(con!=null)
+		{
+			con.setNombre(data.getNombre());
+			con.setTematica(data.getTematica());
+			con.setAsistentes(data.getAsistentes());
+			con.setFecha(data.getDate());
+			return true;
+		}
+		return false;
 	}
 
 	public Object mostrarConferencia(Integer id) {
+		Conferencia con = findConferencia(id);
+		if(con!=null)
+			return con;
 		return null;
 	}
 
 	public List<Object> mostrarTodasLasConferencias() {
+		//Aqui tenemos una movida tamb
+		//TypedQuery<Conferencia> query = em.createQuery("SELECT * FROM Conferencia c", Conferencia.class);
 		return null;
+	}
+	
+	
+	public Conferencia findConferencia(Integer id)
+	{
+		return em.find(Conferencia.class, id);
 	}
 }

5df1901471382a04f03889155cef85316e44cbc6
csegundo <carlossn20165@gmail.com>
2020-03-24 21:37:50 +0100

    Prueba commit

--- GameShopProyect/src/Main/Main.java
+++ GameShopProyect/src/Main/Main.java
@@ -14,9 +14,9 @@ import Presentacion.View.InfoDB;
 public class Main {
 	
 	public static final String applicationName = "GameShop";
-	public static String database = "gameshopapplication";
+	public static String database = "gameshop";
 	public static String user = "root";
-	public static String password = "";
+	public static String password = "1234";
 
 
 	public static void main(String[] args) {

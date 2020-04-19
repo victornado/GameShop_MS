ec48c66f7d4c2f51a5fd397038257c89a75fe68f
joalow <joasm.98@zoho.eu>
2020-04-19 20:30:25 +0200

    fixed problems

---
+++ GameShopProyect/src/Main/Main.java
@@ -0,0 +1,54 @@
+
+package src.Main;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.swing.SwingUtilities;
+
+import Presentacion.View.InfoDB;
+
+public class Main {
+
+	public static final String applicationName = "GameShop";
+	public static String database = "gameshopapplication";
+	public static String user = "root";
+	public static String password = "1234567890";
+
+
+	public static void main(String[] args) {
+
+		try
+		{
+			AudioInputStream audioInputStream = 
+			AudioSystem.getAudioInputStream(new File("resources/gameshop_intro.wav"));/*new File("GameShopProyect" + 
+			System.getProperty("file.separator") + "resources/gameshop_intro.wav").getAbsoluteFile());*/
+			AudioFormat format = audioInputStream.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			Clip sound = (Clip)AudioSystem.getLine(info);
+			//sound.open(audioInputStream);
+
+			//Inicializamos el programa, y ejecutamos el sonido
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					new InfoDB();
+					//Controller.getInstance();
+					//sound.start();
+				}
+			});
+
+		}
+		catch(Exception e)
+		{
+			System.err.println("Error when loading the app");
+		}
+
+
+	}
+
+}

4ab803a4610b9f652f14bc2cbd71905e6e12889f
csegundo <carlossn20165@gmail.com>
2020-05-03 13:37:10 +0200

    finish all

--- GameShopProyect/src/Main/Main.java
+++ GameShopProyect/src/Main/Main.java
@@ -15,9 +15,9 @@ import Presentacion.View.InfoDB;
 public class Main {
 	
 	public static final String applicationName = "GameShop";
-	public static String database = "gameshop";
+	public static String database = "gameshopapplication";
 	public static String user = "root";
-	public static String password = "1234567890";
+	public static String password = "1234";
 
 
 	public static void main(String[] args) {
